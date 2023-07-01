5ee0144c465f3772224eed2457bff0c9de818ca4
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-07-01 18:24:18 +0200

    Adds associations

--- student.rb
+++ student.rb
@@ -1,9 +1,13 @@
-require './person'
+require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
