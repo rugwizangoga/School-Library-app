f61b879be7e497b71468eb8d0337a06cffdda79c
Daniel-Kigozi <kdanalex69@gmail.com>
2023-07-04 17:06:56 +0300

    Add display function

--- main.rb
+++ main.rb
@@ -5,7 +5,18 @@ class Menu
     @app = app
   end
 
-  # display
+  def display
+    puts 'Please choose an option by entering a number'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    puts ''
+    print 'Enter number: '
+  end
 
   # handle_option
 end
